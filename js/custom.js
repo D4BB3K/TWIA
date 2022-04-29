28dd8bc988d6ca0a8e359821ec6e6600d0b1b165
D4BB3K <d4bb3k.biznes@gmail.com>
2022-04-29 15:52:28 +0200

    main

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(function () {
 
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
-	}, 1500);
+	}, 1700);
 
 	/* JQuery Menu
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
